60c0b892ae4aa7fd0ebd6d2fc34a29b71ada23fc
mittjobbkenny <mittjobbkenny@gmail.com>
2023-01-19 11:51:10 +0100

    Add search functionality 2x

--- blog/urls.py
+++ blog/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('<slug:slug>/<int:pk>/', views.PostDetail.as_view(), name='post_detail'),
     path('post/add/', views.PostAdd.as_view(), name='add_post'),
     path('<slug:slug>/<int:pk>/update/', views.PostUpdate.as_view(), name='update_post'),
-    path('<int:pk>/delete/', views.PostDelete.as_view(), name='delete_post')
+    path('<int:pk>/delete/', views.PostDelete.as_view(), name='delete_post'),
+    path("search/", views.SearchResults.as_view(), name="search_results")
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,12 @@
 from django.views import generic
 from django.urls import reverse_lazy
 from django.contrib import messages
+
+from django.shortcuts import redirect
+from django.http import Http404
+
+from django.db.models import Q
+
 from .models import Post
 from .forms import PostForm
 
@@ -43,3 +49,42 @@ class PostDelete(generic.DeleteView):
     def delete(self, request, *args, **kwargs):
         messages.warning(self.request, 'Post deleted')
         return super(PostDelete, self).delete(request, *args, **kwargs)
+
+
+class SearchResults(generic.ListView):
+    model = Post
+    template_name = 'blog/search_results.html'
+    allow_empty = False
+
+    def get_queryset(self):
+        query = self.request.GET.get('q')
+        if query:
+            object_list = Post.objects.filter(
+                Q(title__icontains=query) | Q(content__icontains=query)
+            )
+            if not object_list:
+                messages.warning(self.request, 'No matches found for this search')
+                object_list = Post.objects.none
+            return object_list
+
+    def dispatch(self, *args, **kwargs):
+        try:
+            return super().dispatch(*args, **kwargs)
+        except Http404:
+            messages.warning(self.request, "You didn't enter any search criteria")
+            return redirect('home')
+
+
+# def get_queryset(self):
+#         query = self.request.GET.get('q')
+#         if query:
+#             object_list = Post.objects.filter(
+#                 Q(title__icontains=query) | Q(content__icontains=query)
+#             )
+#             if not object_list:
+#                 messages.warning(self.request, 'No matches found for this search')
+#                 object_list = None
+#         else:
+#             messages.warning(self.request, "You didn't enter any search criteria")
+#             object_list = Post.objects.all()
+#         return object_list
