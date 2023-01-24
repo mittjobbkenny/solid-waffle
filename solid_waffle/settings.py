58990d5ec11f4f9d10eae0ea7a035d3347900b04
mittjobbkenny <mittjobbkenny@gmail.com>
2023-01-24 11:26:07 +0100

    Update language and time zone

--- solid_waffle/settings.py
+++ solid_waffle/settings.py
@@ -129,9 +129,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'sv'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Etc/GMT-1'
 
 USE_I18N = True
 
